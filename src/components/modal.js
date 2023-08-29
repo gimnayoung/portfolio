279c7fe9335649cd65deed219cac6987c5458499
gimnayoung <kny_7_@naver.com>
2023-08-29 12:01:52 +0900

    모달Css

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,28 +1,68 @@
 import { styled } from "styled-components";
+import { RiCloseFill } from "react-icons/ri";
+import { VscChromeMinimize } from "react-icons/vsc";
+import { useState } from "react";
 
-const ModalWrap=styled.div`
-margin: 0 auto;
-width: 500px;
-height: 700px;
-max-height: 100%;
- position: absolute;
-  top: 50%;
-  /* left: 50%; */
+const ModalWrap = styled.div`
+  box-sizing: border-box;
+  border: 1px green solid;
   margin: 0 auto;
-`
-const Wrap=styled.div`
-width: 500px;
-height: 700;
-max-width: 100%;
-background-color: red;
-`
-function Modal(){
-    return(
-       <ModalWrap>
-        <Wrap>
-            안녕 나는 모달
-        </Wrap>
-       </ModalWrap>
-    )
+  width: 20%;
+  min-width: 280px;
+  height: 40%;
+  position: fixed;
+  top: 25%;
+  left: 40%;
+  margin: 0 auto;
+  background-color: white;
+  border-radius: 7px;
+  /* padding: 12px; */
+  box-shadow: rgba(0, 0, 0, 0.5) 0px 0px 10px;
+`;
+const Wrap = styled.div``;
+const Header = styled.div`
+  width: 100%;
+  height: 20px;
+  background-color: gray;
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+`;
+const IconBox = styled.div`
+  width: 45px;
+  border: 1px red solid;
+  display: flex;
+  justify-content: space-around;
+`;
+const State = styled.div`
+  box-sizing: border-box;
+  padding: 2px;
+  display: flex;
+  justify-content: space-between;
+  /* border: 1px red solid; */
+  width: 150px;
+  height: 17px;
+  background-color: white;
+  border-radius: 4px 4px 0 0;
+`;
+const Contents = styled.div``;
+function Modal() {
+  return (
+    <ModalWrap>
+      <Wrap>
+        <Header>
+          <State>
+            <div>내위치</div>
+            <RiCloseFill size={15}></RiCloseFill>
+          </State>
+          <IconBox>
+            <VscChromeMinimize></VscChromeMinimize>
+            <RiCloseFill size={20}></RiCloseFill>
+          </IconBox>
+        </Header>
+        <Contents>내용</Contents>
+      </Wrap>
+    </ModalWrap>
+  );
 }
 export default Modal;
